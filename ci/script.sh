bf17963609cf7406043dcee48931ec1a2dc41ac2
Nick Fitzgerald <fitzgen@gmail.com>
2018-04-17 15:42:53 -0700

    Make sure the right version of wasm-bindgen is installed in CI

--- ci/script.sh
+++ ci/script.sh
@@ -16,7 +16,11 @@ case "$JOB" in
         cd ./wasm-api
         cargo +nightly build --release --target wasm32-unknown-unknown
 
-        test -x ./bin/wasm-bindgen || cargo install wasm-bindgen-cli --version 0.2.3 --root "$(pwd)"
+        # Install wasm-bindgen at the correct version, if necessary.
+        test -x ./bin/wasm-bindgen \
+            && test "$(./bin/wasm-bindgen --version | xargs)" == "wasm-bindgen 0.2.3" \
+                || cargo +nightly install -f wasm-bindgen-cli --version 0.2.3 --root "$(pwd)"
+
         ./bin/wasm-bindgen --out-dir . ../target/wasm32-unknown-unknown/release/twiggy_wasm_api.wasm
         wc -c *.wasm
         ;;
