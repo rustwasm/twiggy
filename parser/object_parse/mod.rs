eef44576feb3289602d3244053bae4ea41d881fd
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-03-26 11:37:48 +0100

    Use symbols rather than debuginfo for finding functions on ELF
    This allows functioning without debuginfo. Some standard library crates
    are intentionally compiled without debuginfo and as such previously
    didn't show up in the report at all or without function name. Same for
    the main function. This also adds statics and anonymous allocations as a
    side effect by not filtering their symbols out.

--- parser/object_parse/mod.rs
+++ parser/object_parse/mod.rs
@@ -1,68 +1,21 @@
 use anyhow::anyhow;
-use fallible_iterator::FallibleIterator;
-use gimli;
-use object::{Object, ObjectSection, ObjectSegment};
-use std::borrow::{Borrow, Cow};
+use object::{
+    elf, Architecture, File, Object, ObjectSection, ObjectSegment, ObjectSymbol, SectionFlags,
+};
 use twiggy_ir as ir;
-use typed_arena::Arena;
 
-mod compilation_unit_parse;
-mod die_parse;
-
-// Helper function used to load a given section of the file.
-fn load_section<'a, 'file, 'input, Sect, Endian>(
-    arena: &'a Arena<Cow<'file, [u8]>>,
-    file: &'file object::File<'input>,
-    endian: Endian,
-) -> Sect
-where
-    Sect: gimli::Section<gimli::EndianSlice<'a, Endian>>,
-    Endian: gimli::Endianity,
-    'file: 'input,
-    'a: 'file,
-{
-    let data = file
-        .section_by_name(Sect::section_name())
-        .and_then(|section| section.uncompressed_data().ok())
-        .unwrap_or(Cow::Borrowed(&[]));
-    let data_ref = (*arena.alloc(data)).borrow();
-    Sect::from(gimli::EndianSlice::new(data_ref, endian))
+fn maybe_thumb_real_addr(file: &File, addr: u64) -> u64 {
+    match file.architecture() {
+        Architecture::Arm => {
+            addr & !1 // Clear LSB. LSB is set when the function is a Thumb function.
+        }
+        _ => addr,
+    }
 }
 
 pub fn parse(data: &[u8]) -> anyhow::Result<ir::Items> {
-    let file: object::File = object::File::parse(data)
-        .map_err(|err| anyhow!("Failed to parse data with err: {:?}", err))?;
-
-    // Identify the file's endianty and create a typed arena to load sections.
-    let arena = Arena::new();
-    let endian = if file.is_little_endian() {
-        gimli::RunTimeEndian::Little
-    } else {
-        gimli::RunTimeEndian::Big
-    };
-
-    // Load the sections of the file containing debugging information.
-    let debug_abbrev: gimli::DebugAbbrev<_> = load_section(&arena, &file, endian);
-    let debug_addr: gimli::DebugAddr<_> = load_section(&arena, &file, endian);
-    let debug_info: gimli::DebugInfo<_> = load_section(&arena, &file, endian);
-    let debug_line: gimli::DebugLine<_> = load_section(&arena, &file, endian);
-    let debug_line_str: gimli::DebugLineStr<_> = load_section(&arena, &file, endian);
-    let debug_str: gimli::DebugStr<_> = load_section(&arena, &file, endian);
-    let debug_str_offsets: gimli::DebugStrOffsets<_> = load_section(&arena, &file, endian);
-    let debug_ranges: gimli::DebugRanges<_> = load_section(&arena, &file, endian);
-    let debug_rnglists: gimli::DebugRngLists<_> = load_section(&arena, &file, endian);
-    let ranges = gimli::RangeLists::new(debug_ranges, debug_rnglists);
-    let dwarf = gimli::Dwarf {
-        debug_abbrev,
-        debug_addr,
-        debug_info,
-        debug_line,
-        debug_line_str,
-        debug_str,
-        debug_str_offsets,
-        ranges,
-        ..Default::default()
-    };
+    let file: File =
+        File::parse(data).map_err(|err| anyhow!("Failed to parse data with err: {:?}", err))?;
 
     let mut alloc_size = 0;
     for segment in file.segments() {
@@ -70,35 +23,54 @@ pub fn parse(data: &[u8]) -> anyhow::Result<ir::Items> {
     }
 
     let mut items = ir::ItemsBuilder::new(alloc_size as u32);
-    parse_items(&mut items, &dwarf)?;
-    parse_edges(&mut items, &dwarf)?;
+
+    for symbol in file.symbols() {
+        if !symbol.is_definition() {
+            continue;
+        }
+
+        if symbol.size() == 0 {
+            continue;
+        }
+
+        // Filter out symbols in non-allocated sections. Their symbol values do not correspond to
+        // actual runtime addresses.
+        match file
+            .section_by_index(symbol.section_index().unwrap())
+            .unwrap()
+            .flags()
+        {
+            SectionFlags::Elf { sh_flags } => {
+                if sh_flags as u32 & elf::SHF_ALLOC != elf::SHF_ALLOC {
+                    continue;
+                }
+            }
+            _ => {}
+        }
+
+        if !file.segments().any(|segment| {
+            segment
+                .data_range(maybe_thumb_real_addr(&file, symbol.address()), 1)
+                .ok()
+                .flatten()
+                .is_some()
+        }) {
+            // Symbol not part of any loaded segment
+            continue;
+        }
+
+        let id = ir::Id::entry(symbol.section_index().unwrap().0, symbol.index().0);
+        let name = symbol.name().unwrap();
+        let kind: ir::ItemKind = ir::Code::new(name).into();
+        let item = ir::Item::new(id, name, symbol.size() as u32, kind);
+        if maybe_thumb_real_addr(&file, symbol.address())
+            == maybe_thumb_real_addr(&file, file.entry())
+        {
+            items.add_root(item);
+        } else {
+            items.add_item(item);
+        }
+    }
+
     Ok(items.finish())
-}
-
-fn parse_items<R: gimli::Reader>(
-    items: &mut ir::ItemsBuilder,
-    dwarf: &gimli::Dwarf<R>,
-) -> anyhow::Result<()> {
-    // Parse the items in each compilation unit.
-    let mut headers = dwarf.units().enumerate();
-    while let Some((unit_id, header)) = headers.next()? {
-        let unit = dwarf.unit(header)?;
-        compilation_unit_parse::parse_items(items, dwarf, &unit, unit_id)?
-    }
-
-    Ok(())
-}
-
-fn parse_edges<R: gimli::Reader>(
-    items: &mut ir::ItemsBuilder,
-    dwarf: &gimli::Dwarf<R>,
-) -> anyhow::Result<()> {
-    // Parse the edges in each compilation unit.
-    let mut headers = dwarf.units().enumerate();
-    while let Some((unit_id, header)) = headers.next()? {
-        let unit = dwarf.unit(header)?;
-        compilation_unit_parse::parse_edges(items, &unit, unit_id)?
-    }
-
-    Ok(())
 }
