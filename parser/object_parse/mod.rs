739cd0463077a740cf18e16520b381a0d292b830
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-03-31 15:39:23 +0200

    Much better support for ELF files (#755)
    * Rebuild hello_elf using rustc 1.85.1 (4eb161250 2025-03-15)
    
    * Update object and gimli
    
    * Use runtime loaded size as total for ELF files
    
    If you care about binary size you are probably going to strip all
    debuginfo and symbols anyway. Unlike wasm, for elf twiggy requires
    debuginfo so people can't strip the executable before running twiggy on
    it and as such using the file size as total is misleading.
    
    * Use symbols rather than debuginfo for finding functions on ELF
    
    This allows functioning without debuginfo. Some standard library crates
    are intentionally compiled without debuginfo and as such previously
    didn't show up in the report at all or without function name. Same for
    the main function. This also adds statics and anonymous allocations as a
    side effect by not filtering their symbols out.
    
    * Handle call edges for ELF files based on relocations
    
    This also nicely handles edges involving statics and anonymous
    allocations too.
    
    * Add warning when there are no relocations
    
    * Regenerate without debuginfo
    
    * Rustfmt
    
    * Update proc-macro2

--- parser/object_parse/mod.rs
+++ parser/object_parse/mod.rs
@@ -1,97 +1,221 @@
+use std::convert::TryInto;
+
 use anyhow::anyhow;
-use fallible_iterator::FallibleIterator;
-use gimli;
-use object::{self, Object};
-use std::borrow::{Borrow, Cow};
+use object::{
+    elf, Architecture, BinaryFormat, Endianness, File, Object, ObjectSection, ObjectSegment,
+    ObjectSymbol, Relocation, RelocationFlags, RelocationTarget, SectionFlags, Symbol, SymbolIndex,
+    SymbolKind,
+};
 use twiggy_ir as ir;
-use typed_arena::Arena;
 
-mod compilation_unit_parse;
-mod die_parse;
-
-// Helper function used to load a given section of the file.
-fn load_section<'a, 'file, 'input, Sect, Endian>(
-    arena: &'a Arena<Cow<'file, [u8]>>,
-    file: &'file object::File<'input>,
-    endian: Endian,
-) -> Sect
-where
-    Sect: gimli::Section<gimli::EndianSlice<'a, Endian>>,
-    Endian: gimli::Endianity,
-    'file: 'input,
-    'a: 'file,
-{
-    let data = file
-        .section_data_by_name(Sect::section_name())
-        .unwrap_or(Cow::Borrowed(&[]));
-    let data_ref = (*arena.alloc(data)).borrow();
-    Sect::from(gimli::EndianSlice::new(data_ref, endian))
+fn maybe_thumb_real_addr(file: &File, addr: u64) -> u64 {
+    match file.architecture() {
+        Architecture::Arm => {
+            addr & !1 // Clear LSB. LSB is set when the function is a Thumb function.
+        }
+        _ => addr,
+    }
 }
 
-pub fn parse(items: &mut ir::ItemsBuilder, data: &[u8]) -> anyhow::Result<()> {
-    let file: object::File = object::File::parse(data)
-        .map_err(|err| anyhow!("Failed to parse data with err: {:?}", err))?;
+pub fn parse(data: &[u8]) -> anyhow::Result<ir::Items> {
+    let file: File =
+        File::parse(data).map_err(|err| anyhow!("Failed to parse data with err: {:?}", err))?;
 
-    // Identify the file's endianty and create a typed arena to load sections.
-    let arena = Arena::new();
-    let endian = if file.is_little_endian() {
-        gimli::RunTimeEndian::Little
+    let mut alloc_size = 0;
+    for segment in file.segments() {
+        alloc_size += segment.size();
+    }
+
+    let mut items = ir::ItemsBuilder::new(alloc_size as u32);
+
+    let mut symbols = vec![];
+    for symbol in file.symbols() {
+        if !symbol.is_definition() {
+            continue;
+        }
+
+        if symbol.size() == 0 {
+            continue;
+        }
+
+        // Filter out symbols in non-allocated sections. Their symbol values do not correspond to
+        // actual runtime addresses.
+        match file
+            .section_by_index(symbol.section_index().unwrap())
+            .unwrap()
+            .flags()
+        {
+            SectionFlags::Elf { sh_flags } => {
+                if sh_flags as u32 & elf::SHF_ALLOC != elf::SHF_ALLOC {
+                    continue;
+                }
+            }
+            _ => {}
+        }
+
+        if !file.segments().any(|segment| {
+            segment
+                .data_range(maybe_thumb_real_addr(&file, symbol.address()), 1)
+                .ok()
+                .flatten()
+                .is_some()
+        }) {
+            // Symbol not part of any loaded segment
+            continue;
+        }
+
+        symbols.push((symbol.address(), symbol.size(), symbol.index()));
+
+        let id = ir::Id::entry(symbol.section_index().unwrap().0, symbol.index().0);
+        let name = symbol.name().unwrap();
+        let kind: ir::ItemKind = ir::Code::new(name).into();
+        let item = ir::Item::new(id, name, symbol.size() as u32, kind);
+        if maybe_thumb_real_addr(&file, symbol.address())
+            == maybe_thumb_real_addr(&file, file.entry())
+        {
+            items.add_root(item);
+        } else {
+            items.add_item(item);
+        }
+    }
+
+    if let BinaryFormat::Elf = file.format() {
+        let mut any_relocs = false;
+        for section in file.sections() {
+            if section.name().unwrap().starts_with(".debug")
+                || section.name().unwrap().starts_with(".eh_frame")
+            {
+                continue;
+            }
+
+            for (offset, reloc) in section.relocations() {
+                any_relocs = true;
+                edge_for_reloc(&file, &mut items, &symbols, offset, reloc);
+            }
+        }
+
+        if !any_relocs {
+            eprintln!(
+                "Warning: Couldn't find any relocations. \
+                 The dominators, garbage and paths subcommands will not function correctly.\n\
+                 Hint: Try recompiling the binary with --emit-relocs.\n"
+            );
+        }
     } else {
-        gimli::RunTimeEndian::Big
-    };
-
-    // Load the sections of the file containing debugging information.
-    let debug_abbrev: gimli::DebugAbbrev<_> = load_section(&arena, &file, endian);
-    let debug_addr: gimli::DebugAddr<_> = load_section(&arena, &file, endian);
-    let debug_info: gimli::DebugInfo<_> = load_section(&arena, &file, endian);
-    let debug_line: gimli::DebugLine<_> = load_section(&arena, &file, endian);
-    let debug_line_str: gimli::DebugLineStr<_> = load_section(&arena, &file, endian);
-    let debug_str: gimli::DebugStr<_> = load_section(&arena, &file, endian);
-    let debug_str_offsets: gimli::DebugStrOffsets<_> = load_section(&arena, &file, endian);
-    let debug_ranges: gimli::DebugRanges<_> = load_section(&arena, &file, endian);
-    let debug_rnglists: gimli::DebugRngLists<_> = load_section(&arena, &file, endian);
-    let ranges = gimli::RangeLists::new(debug_ranges, debug_rnglists);
-    let dwarf = gimli::Dwarf {
-        debug_abbrev,
-        debug_addr,
-        debug_info,
-        debug_line,
-        debug_line_str,
-        debug_str,
-        debug_str_offsets,
-        ranges,
-        ..Default::default()
-    };
-
-    parse_items(items, &dwarf)?;
-    parse_edges(items, &dwarf)?;
-    Ok(())
-}
-
-fn parse_items<R: gimli::Reader>(
-    items: &mut ir::ItemsBuilder,
-    dwarf: &gimli::Dwarf<R>,
-) -> anyhow::Result<()> {
-    // Parse the items in each compilation unit.
-    let mut headers = dwarf.units().enumerate();
-    while let Some((unit_id, header)) = headers.next()? {
-        let unit = dwarf.unit(header)?;
-        compilation_unit_parse::parse_items(items, dwarf, &unit, unit_id)?
+        eprintln!(
+            "Warning: Note: The dominators, garbage and paths subcommands currently only support \
+                WASM and ELF.\n"
+        )
     }
 
-    Ok(())
+    Ok(items.finish())
 }
 
-fn parse_edges<R: gimli::Reader>(
-    items: &mut ir::ItemsBuilder,
-    dwarf: &gimli::Dwarf<R>,
-) -> anyhow::Result<()> {
-    // Parse the edges in each compilation unit.
-    let mut headers = dwarf.units().enumerate();
-    while let Some((unit_id, header)) = headers.next()? {
-        let unit = dwarf.unit(header)?;
-        compilation_unit_parse::parse_edges(items, &unit, unit_id)?
+fn read_at<const N: usize>(file: &File<'_>, offset: u64) -> [u8; N] {
+    file.segments()
+        .find_map(|segment| segment.data_range(offset, N as u64).unwrap())
+        .unwrap()
+        .try_into()
+        .unwrap()
+}
+
+fn edge_for_reloc(
+    file: &File<'_>,
+    items: &mut twiggy_ir::ItemsBuilder,
+    symbols: &Vec<(u64, u64, SymbolIndex)>,
+    offset: u64,
+    reloc: Relocation,
+) {
+    let Some(reloc_source) = symbol_for_addr(file, symbols, offset) else {
+        return;
+    };
+
+    match reloc.target() {
+        // If the reloc is relative to a non-section symbol, we can directly use this symbol as target.
+        RelocationTarget::Symbol(reloc_target_idx)
+            if file.symbol_by_index(reloc_target_idx).unwrap().kind() != SymbolKind::Section =>
+        {
+            if !symbols.iter().any(|&(_, _, idx)| idx == reloc_target_idx) {
+                return;
+            }
+            let reloc_target = file.symbol_by_index(reloc_target_idx).unwrap();
+            add_edge_for_symbol(items, reloc_source, reloc_target);
+            return;
+        }
+
+        // Otherwise we need to compute the target address and find the symbol covering this address.
+        _ => {}
     }
 
-    Ok(())
+    let implicit_addend = match file.architecture() {
+        Architecture::Arm => {
+            assert_eq!(file.endianness(), Endianness::Little);
+            assert!(reloc.has_implicit_addend());
+            match reloc.flags() {
+                RelocationFlags::Elf {
+                    r_type: elf::R_ARM_ABS32,
+                } => u64::from(u32::from_le_bytes(read_at(file, offset))) as i64,
+                ty => todo!("{:?}", ty),
+            }
+        }
+        Architecture::X86_64 => {
+            assert!(!reloc.has_implicit_addend());
+            match reloc.flags() {
+                RelocationFlags::Elf {
+                    r_type: elf::R_X86_64_PC32 | elf::R_X86_64_PLT32,
+                } => 4,
+                RelocationFlags::Elf {
+                    r_type: elf::R_X86_64_64,
+                } => 0,
+                ty => todo!("{:?}", ty),
+            }
+        }
+        arch => todo!("relocations for architecture {:?} not yet supported", arch),
+    };
+
+    let symbol_addr = match reloc.target() {
+        RelocationTarget::Symbol(reloc_target_idx) => {
+            file.symbol_by_index(reloc_target_idx).unwrap().address()
+        }
+        RelocationTarget::Absolute => 0,
+        _ => todo!(),
+    };
+
+    let target_addr = (symbol_addr as i64 + reloc.addend() + implicit_addend) as u64;
+    let Some(reloc_target) = symbol_for_addr(file, symbols, target_addr) else {
+        return;
+    };
+    add_edge_for_symbol(items, reloc_source, reloc_target);
+}
+
+fn symbol_for_addr<'data, 'file>(
+    file: &'file File<'data>,
+    symbols: &Vec<(u64, u64, SymbolIndex)>,
+    offset: u64,
+) -> Option<Symbol<'data, 'file>> {
+    let Some(&(_, _, reloc_source_idx)) = symbols
+        .iter()
+        .find(|&&(addr, size, _idx)| (addr..addr + size).contains(&offset))
+    else {
+        return None;
+    };
+
+    Some(file.symbol_by_index(reloc_source_idx).unwrap())
+}
+
+fn add_edge_for_symbol(
+    items: &mut twiggy_ir::ItemsBuilder,
+    reloc_source: Symbol<'_, '_>,
+    reloc_target: Symbol<'_, '_>,
+) {
+    items.add_edge(
+        ir::Id::entry(
+            reloc_source.section_index().unwrap().0,
+            reloc_source.index().0,
+        ),
+        ir::Id::entry(
+            reloc_target.section_index().unwrap().0,
+            reloc_target.index().0,
+        ),
+    );
 }
--- twiggy/tests/all/elf_format_tests.rs
+++ twiggy/tests/all/elf_format_tests.rs
@@ -7,3 +7,52 @@ test!(
 );
 
 test!(elf_top_hello_world_rs, "top", "./fixtures/hello_elf");
+
+test!(
+    elf_paths,
+    "paths",
+    "./fixtures/hello_elf",
+    "addr2line::render_file::h8b2b27d4ac1b7166",
+    "-d",
+    "3" //"-f",
+        //"json"
+);
+
+test!(
+    elf_paths2,
+    "paths",
+    "./fixtures/hello_elf",
+    "main",
+    "-d",
+    "3" //"-f",
+        //"json"
+);
+
+test!(
+    elf_dominators,
+    "dominators",
+    "./fixtures/hello_elf",
+    "-d",
+    "3" //"-f",
+        //"json"
+);
+
+test!(
+    elf_dominators2,
+    "dominators",
+    "./fixtures/hello_elf",
+    "main",
+    "-d",
+    "3" //"-f",
+        //"json"
+);
+
+test!(
+    elf_dominators3,
+    "dominators",
+    "./fixtures/hello_elf",
+    "rust_panic",
+    "-d",
+    "3" //"-f",
+        //"json"
+);
