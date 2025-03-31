9eec61b3157ee8aeba8fb16ec41059338a10d6cd
bjorn3 <17426603+bjorn3@users.noreply.github.com>
2025-03-31 14:00:15 +0200

    Regenerate without debuginfo

--- twiggy/tests/all/fixtures/hello_world.rs
+++ twiggy/tests/all/fixtures/hello_world.rs
@@ -1,13 +1,13 @@
 //! Rebuild `hello_elf` with:
 //!
 //! ```
-//! rustc -g --target x86_64-unknown-linux-gnu hello_world.rs -o hello_elf -C lto=fat -C opt-level=z -Clink-arg=-Wl,--emit-relocs
+//! rustc --target x86_64-unknown-linux-gnu hello_world.rs -o hello_elf -C lto=fat -C opt-level=z -Clink-arg=-Wl,--emit-relocs
 //! ```
 //!
 //! Rebuild `hello_mach` with:
 //!
 //! ```
-//! rustc +nightly -g --target x86_64-apple-darwin hello_world.rs -o hello_mach.o -C lto=fat -C opt-level=z
+//! rustc +nightly --target x86_64-apple-darwin hello_world.rs -o hello_mach.o -C lto=fat -C opt-level=z
 //! ```
 //! NOTE: The above is not working for me on Ubuntu. This causes an error when `ld` is invoked.
 
